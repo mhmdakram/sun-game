cdddb0608676ecfcfcbf0c08d701a0dd87fb3384
mhmdakram <mhmdakram111@gmail.com>
2022-12-24 17:03:11 +0530

    Updated

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-function aniLeft() {
-    document.getElementById('sun').className = 'moveLeft';
-    document.getElementById('light').className = 'moveLeftLight';
-}
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+$(document).ready(function () {
+    $("#aniLeft").click(function () {
+        $("#sun").addClass("aniLeft") && $("#light").addClass("aniLeftLight") && $("#sun").removeClass("aniRight") && $("#light").removeClass("aniRightLight");
+    });
+
+    $("#aniRight").click(function () {
+        $("#light").removeClass("aniLeftLight") && $("#sun").removeClass("aniLeft") && $("#light").addClass("aniRightLight") && $("#sun").addClass("aniRight");
+    });
+});
