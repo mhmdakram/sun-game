644b590e63d789aac3bdb69f7fd4937e1ff22dc8
mhmdakram <mhmdakram111@gmail.com>
2022-12-26 11:58:56 +0530

    Updated

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 
 
+
 $(document).ready(function () {
     $("#aniLeft").click(function () {
         $("#sun").addClass("aniLeft") && $("#light").addClass("aniLeftLight") && $("#sun").removeClass("aniRight") && $("#light").removeClass("aniRightLight");
+
     });
 
     $("#aniRight").click(function () {
         $("#light").removeClass("aniLeftLight") && $("#sun").removeClass("aniLeft") && $("#light").addClass("aniRightLight") && $("#sun").addClass("aniRight");
+
     });
+
+
+
 });
 
 
